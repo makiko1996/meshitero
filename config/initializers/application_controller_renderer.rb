1ca9a654357e4b94c48be842b360a217431cf5fa
makikoTakemori <richikaemay@gmail.com>
2021-11-15 10:47:35 +0000

    [update]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '4f11c9baf0e36f084e23a8f2373b64c189bdcef7a571b82951ba9aa7338237c72531f246a0f18fe8aec8b70e5eaec9c238603c86ee01f2b66cabd6b39fc0dfbc'
